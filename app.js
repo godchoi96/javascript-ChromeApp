bdf61a37dfb93b81e6ce577db8bb9ac58742436d
CSD <80474006+godchoi96@users.noreply.github.com>
2021-07-25 11:09:55 +0900

    21.07.25 const

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const a = 5;
 const b = 2;
-const myName = "CSD";
+let myName = "CSD";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("Hello" + myName);
+
+myName = "Choi Seong Dae";
+
+console.log("your new name is " + myName);
