fbe087b7c44038c54ed16b8354938252fa17fc91
CSD <80474006+godchoi96@users.noreply.github.com>
2021-08-20 21:50:26 +0900

    21.08.20

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
-const cal = {
-  add: function (num1, num2) {
-    console.log(num1 + num2);
-  },
-  minus: function (num1, num2) {
-    console.log(num1 - num2);
-  },
-  multip: function (num1, num2) {
-    console.log(num1 * num2);
-  },
-  divide: function (num1, num2) {
-    console.log(num1 / num2);
-  },
-};
+const title = document.querySelector("div.hello:first-child h1");
 
-cal.add(5, 6);
-cal.minus(5, 6);
-cal.multip(5, 6);
-cal.divide(5, 6);
+function handleTitleClick() {
+  title.style.color = "blue";
+}
+
+function handleMouseEnter() {
+  title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is Gone!";
+}
+
+function handleWindowsResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copier!");
+}
+
+function handleWindowOffline() {
+  alert("SOS NO WIFI");
+}
+
+function handleWindowOnline() {
+  alert("ALL GOOD!!");
+}
+
+title.onclick = handleTitleClick;
+title.onmouseenter = handleMouseEnter;
+title.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowsResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventLisnter("Online", handleWindowOnline);
