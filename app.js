327ceeb7543016c05f751020e46f909661399850
CSD <80474006+godchoi96@users.noreply.github.com>
2021-08-20 22:02:06 +0900

    21.08.20 +

--- app.js
+++ app.js
@@ -1,38 +1,14 @@
 const title = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  title.style.color = "blue";
+  const currentColor = title.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  title.style.color = newColor;
 }
 
-function handleMouseEnter() {
-  title.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-  title.innerText = "Mouse is Gone!";
-}
-
-function handleWindowsResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier!");
-}
-
-function handleWindowOffline() {
-  alert("SOS NO WIFI");
-}
-
-function handleWindowOnline() {
-  alert("ALL GOOD!!");
-}
-
-title.onclick = handleTitleClick;
-title.onmouseenter = handleMouseEnter;
-title.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowsResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventLisnter("Online", handleWindowOnline);
+title.addEventListener("click", handleTitleClick);
