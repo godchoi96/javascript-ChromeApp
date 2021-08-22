d0daf7c8dd45cf6f2cebfd48b755b508970e6b92
CSD <80474006+godchoi96@users.noreply.github.com>
2021-08-22 22:46:12 +0900

    21.08.22

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const title = document.querySelector("div.hello:first-child h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function handleTitleClick() {
-  const clickedClass = "clicked";
-  if (title.classList.contains(clickedClass)) {
-    title.classList.remove(clickedClass);
-  } else {
-    title.classList.add(clickedClass);
-  }
-  // 위 다섯 문장을 title.classList.toggle("clicked"); 로 대체 가능
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, loginInput.value);
+  paintGreetings();
 }
 
-title.addEventListener("click", handleTitleClick);
+function paintGreetings() {
+  const username = localStorage.getItem(USERNAME_KEY);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings();
+}
