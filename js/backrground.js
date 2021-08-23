43fbabd043d1b92074c56ead6800d89177e6e90f
CSD <80474006+godchoi96@users.noreply.github.com>
2021-08-24 08:10:39 +0900

    21.08.24 +

---
+++ js/backrground.js
@@ -0,0 +1,9 @@
+const images = ["0.jpg", "1.jpeg", "2.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img"); // 자바스크립트에서 img 요소를 생성
+
+bgImage.src = `img/${chosenImage}`; // html의 img 태그는 img/ 형식이기 때문에 img/ + {chosenimage}로 만든다.
+
+document.body.appendChild(bgImage); // 생성한 img 태그를 html의 body 부분에 넣는다.
